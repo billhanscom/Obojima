a41a82f5e01b4eb83046019bc1956f98b843afca
Bill Hanscom <43036581+billhanscom@users.noreply.github.com>
2024-11-04 14:11:46 -0500

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -19,20 +19,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function findRecipes() {
-    // Display alert if less than three ingredients are selected
     if (selectedIngredients.length < 3) {
         alert("Oops! Please select at least three ingredients.");
         return;
     }
 
-    // Send selected ingredients to the backend
     const response = await fetch('/get-recipes', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ ingredients: selectedIngredients })
     });
 
-    // Display each possible recipe from the response in columns by potion type
     const recipes = await response.json();
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
@@ -46,13 +43,13 @@ async function findRecipes() {
     ["Combat", "Utility", "Whimsy"].forEach(type => {
         const column = document.createElement('div');
         column.classList.add('recipe-column');
-        column.innerHTML = `<h3>${columnHeaders[type]}</h3>`;
+        column.innerHTML = `<h3 class="recipe-title">${columnHeaders[type]}</h3>`;
 
         if (recipes[type] && recipes[type].length > 0) {
             column.innerHTML += recipes[type].map(recipe => {
                 const ingredientsList = recipe.ingredients.map(ing => {
-                    const rarityClass = ing.rarity.toLowerCase();  // Apply rarity class to ingredient background
-                    return `<li class="ingredient ${rarityClass}">${ing.name} [${ing.combat}-${ing.utility}-${ing.whimsy}]</li>`;
+                    const rarityClass = ing.rarity.toLowerCase();
+                    return `<li class="ingredient ${rarityClass}">${ing.name} [${ing.combat}/${ing.utility}/${ing.whimsy}]</li>`;
                 }).join('');
                 return `<h4>${recipe.potion_type} ${recipe.attribute_totals}</h4><ul>${ingredientsList}</ul>`;
             }).join('');
@@ -62,15 +59,13 @@ async function findRecipes() {
         resultsDiv.appendChild(column);
     });
 
-    // Automatically scroll to the results section
     document.getElementById('results').scrollIntoView({ behavior: 'smooth' });
 }
 
-// Function to clear selected ingredients
 function clearSelection() {
     selectedIngredients = [];
     document.querySelectorAll(".ingredient-button").forEach(button => {
         button.classList.remove("selected", "common", "uncommon", "rare");
     });
-    document.getElementById("results").innerHTML = ''; // Clear results as well
+    document.getElementById("results").innerHTML = '';
 }
